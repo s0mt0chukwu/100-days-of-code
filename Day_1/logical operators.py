f0419e097438d29b70d7f6b8192458d5933c8507
s0mt0chukwu <somtochukwup732@gmail.com>
2026-09-04 11:26:05 -1100

    Day_1 completed

--- Day_1/logical operators.py
+++ Day_1/logical operators.py
@@ -1,9 +1,9 @@
 print("welcome to  the tip calculator!")
 bill = float(input("what was the total bill? $"))
-tip = int(input("what percentage tip would you like to give? 10 12 15"))
+tip = int(input("what percentage tip would you like to give? 10 12 15: "))
 people = int(input("how many people to split the bill? "))
 tip_with_percent = tip/100
-total_tip_amount = bill + tip_with_percent
+total_tip_amount = bill * tip_with_percent
 total_bill =  bill + total_tip_amount
 bill_per_person = total_bill/people
 final_amount = round(bill_per_person,2)
